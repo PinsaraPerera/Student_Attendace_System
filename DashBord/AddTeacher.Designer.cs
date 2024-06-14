abe9176ed64850a7a1ec89055885b61199fbdf8c
Viduruwan-55 <ypviduruwan55@gmail.com>
2024-06-14 18:53:09 +0530

    grid view create

--- DashBord/AddTeacher.Designer.cs
+++ DashBord/AddTeacher.Designer.cs
@@ -78,7 +78,6 @@ namespace DashBord
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1409, 62);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
             // 
             // create_lecture_btn
             // 
