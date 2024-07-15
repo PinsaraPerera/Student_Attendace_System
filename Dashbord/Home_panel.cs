7fa07d1f29f1e21d3f6ffb1bd6badea41838be7e
DulithKavinda <121927516+DulithKavinda@users.noreply.github.com>
2024-07-15 17:12:25 +0530

    Add files via upload
    Adding Dashboard design

--- Dashbord/Home_panel.cs
+++ Dashbord/Home_panel.cs
@@ -16,5 +16,50 @@ namespace Student_Attendace_System.Dashbord
         {
             InitializeComponent();
         }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaGradientPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_panel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaLabel6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void gunaLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2GradientPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
